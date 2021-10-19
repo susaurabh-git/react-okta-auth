14c5f64cfb40f5d59031601c26b9d1f77e7489e6
Suman Saurabh <susaurabh@gmail.com>
2021-10-19 14:38:34 -0400

    Removed unused code

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -1,13 +1,8 @@
-import AuthService from "../services/AuthService";
 const SCOPE = "oktaAuth/";
 export const LOGIN = `${SCOPE}LOGIN`;
 export const UPDATE = `${SCOPE}UPDATE`;
 
 export const authAction = {
-  login: (username, password) => ({
-    type: LOGIN,
-    payload: { promise: AuthService.login(username, password) },
-  }),
   update: (transaction) => ({
     type: UPDATE,
     payload: transaction,
